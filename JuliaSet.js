ec99ed109f56b7d8ac2af6357f6bd2e4ffd82a9d
HectorSchreiner <agenthector007@gmail.com>
2022-02-24 20:22:04 +0100

    almost done

--- JuliaSet.js
+++ JuliaSet.js
@@ -1,19 +1,30 @@
 let cRe, cIm;
-let maxIterations;
+let maxIterations_Julia, maxIterations_Mandelbrot, _iterator = 0;
+let canvasOffset = 50;
+
+let zoom = 1;
+let zoom_speed = 2;
+let offX = 0;
+let offY = 0;
+
+let iterSlider, buttonAdd, buttonSub;
+
 
 let juliaSet = function (sketch) {
 
-  let newRe, newIm, oldRe, oldIm;
-  maxIterations = 20;
+  iterSlider = sketch.createSlider(8, 256, 20, 8)
 
-  let width;
-  let height;
-  let zoom = 1;
-  let offX = 0;
-  let offY = 0;
+  buttonAdd = sketch.createButton("Zoom +");
+  buttonSub = sketch.createButton("Zoom -")
+  buttonAdd.mouseClicked(() => zoom += zoom_speed);
+  buttonSub.mouseClicked(() => zoom -= zoom_speed);
+
+
 
   sketch.setup = function () {
-    let canvas1 = sketch.createCanvas(600, 600);
+    let canvas1 = sketch.createCanvas(400, 400);
+    canvas1.position(width, canvasOffset);
+
     sketch.pixelDensity(1);
 
     cRe = -0.7;
@@ -25,79 +36,125 @@ let juliaSet = function (sketch) {
 
   sketch.draw = function () {
 
+    maxIterations_Julia = iterSlider.value();
+
+
     sketch.background(20);
 
     // initialiserer pixels array
     sketch.loadPixels();
 
     // går igennem hver pixel på canvas og farver derefter
+
+    // -----------------------------------------
+    // * TODO Optimiser til kun pixels in view *
+    // -----------------------------------------
+
     for (let y = 0; y < height; y++) {
       for (let x = 0; x < width; x++) {
 
-        newRe = 1.5 * (x - width / 2) / (0.5 * width * zoom) + offX;
-        newIm = (y - height / 2) / (0.5 * height * zoom) + offY;
-        let i;
-        for (i = 0; i < maxIterations; i++) {
+        let i = calculateJuliaSet(x, y, maxIterations_Julia, sketch);
 
-          // husker values fra tidligere iteration
-          oldRe = newRe;
-          oldIm = newIm;
-          newRe = oldRe * oldRe - oldIm * oldIm + cRe;
-          newIm = 2 * oldRe * oldIm + cIm;
+        // newRe = 1.5 * (x - width / 2) / (0.5 * width * sketch.pow(1.1, zoom)) + offX;
+        // newIm = (y - height / 2) / (0.5 * height * sketch.pow(1.1, zoom)) + offY;
 
-          // hvis punktet er uden for cirklen med radius 2 -> stop
-          if ((newRe * newRe + newIm * newIm) > 4) {
-            break;
-          }
+        // for (i = 0; i < maxIterations_Julia; i++) {
 
-        }
+        //   // husker values fra tidligere iteration
+        //   oldRe = newRe;
+        //   oldIm = newIm;
+        //   newRe = oldRe * oldRe - oldIm * oldIm + cRe;
+        //   newIm = 2 * oldRe * oldIm + cIm;
 
-        let color_val_julia = sketch.map(i, 0, maxIterations, 0, 255);
-        colorPixel(x, y, color_val_julia, color_val_julia, color_val_julia, 255, sketch);
+        //   // hvis punktet er uden for cirklen med radius 2 -> stop
+        //   if ((newRe * newRe + newIm * newIm) > 4) {
+        //     break;
+        //   }
+
+        // }
+
+        let color_val_julia = sketch.map(i, 0, maxIterations_Julia, 0, 255);
+
+        colorPixel(x, y, color_val_julia, color_val_julia, color_val_julia, sketch);
       }
     }
 
     sketch.updatePixels();
 
     drawText(sketch);
-  }
 
-  // farver en given pixel(x,y) med rgb value
-
-
-  sketch.mouseClicked = function () {
-    getConstantValues();
-  }
-
-  function getConstantValues() {
-    cRe = -1 + sketch.mouseX / width * 2;
-    cIm = -1 + sketch.mouseY / height * 2;
+    //console.log(sketch.frameRate());
   }
 }
 
 new p5(juliaSet);
 
-function colorPixel(x, y, r, g, b, alpha, sketch) {
+// farver en given pixel(x,y) med rgb value
+function colorPixel(x, y, r, g, b, sketch) {
 
   // pixel index 
   let index = (x + y * sketch.width) * 4;
   sketch.pixels[index] = r;
   sketch.pixels[index + 1] = g;
   sketch.pixels[index + 2] = b;
-  sketch.pixels[index + 3] = alpha;
+  sketch.pixels[index + 3] = 255;
 }
 
 function drawText(sketch) {
   let textString = "C = ";
-  textString += sketch.str(cRe);
+  textString += sketch.str(cRe.toFixed(3));
   textString += " + ";
-  textString += sketch.str(cIm);
+  textString += sketch.str(cIm.toFixed(3));
   textString += " i";
 
   sketch.fill("white");
   sketch.textSize(16);
   sketch.text(textString, 10, 20);
 }
+
+
+// newRe = 1.5 * (x - width / 2) / (0.5 * width * sketch.pow(1.1, zoom)) + offX;
+// newIm = (y - height / 2) / (0.5 * height * sketch.pow(1.1, zoom)) + offY;
+
+// for (i = 0; i < maxIterations_Julia; i++) {
+
+//   // husker values fra tidligere iteration
+//   oldRe = newRe;
+//   oldIm = newIm;
+//   newRe = oldRe * oldRe - oldIm * oldIm + cRe;
+//   newIm = 2 * oldRe * oldIm + cIm;
+
+//   // hvis punktet er uden for cirklen med radius 2 -> stop
+//   if ((newRe * newRe + newIm * newIm) > 4) {
+//     break;
+//   }
+
+// }
+
+function calculateJuliaSet(x, y, maxDepth, sketch) {
+  function recurseJuliaSet(re, im, cDepth) {
+
+    if (cDepth == maxDepth) {
+      return maxDepth;
+    }
+
+    const reNew = re * re - im * im + cRe;
+    const imNew = 2 * re * im + cIm;
+
+    if (reNew * reNew + imNew * imNew > 4) {
+      return cDepth;
+    }
+
+    return recurseJuliaSet(reNew, imNew, cDepth + 1);
+  }
+
+  re = 1.5 * (x - width / 2) / (0.5 * width * sketch.pow(1.1, zoom)) + offX;
+  im = (y - height / 2) / (0.5 * height * sketch.pow(1.1, zoom)) + offY;
+
+  return recurseJuliaSet(re, im, 0);
+}
+
+
 
 
 
--- MandelbrotSet.js
+++ MandelbrotSet.js
@@ -1,60 +1,137 @@
 let height, width;
-let minVal, maxval;
+let minVal, maxVal;
+let marker;
+let iterSlider_mandelbrot;
 
 
 let mandelbrotSet = function (sketch) {
     sketch.setup = function () {
 
+        iterSlider_mandelbrot = sketch.createSlider(8, 256, 20, 8);
         minVal = -2;
         maxVal = 2;
 
         sketch.pixelDensity(1);
 
-        let canvas = sketch.createCanvas(600, 600);
-        canvas.position(600, 0);
+        let canvas = sketch.createCanvas(400, 400);
 
-        sketch.background(20);
         height = sketch.height;
         width = sketch.width;
+
+        canvas.position(width, canvasOffset);
+        iterSlider_mandelbrot.position(width, 0);
+
+        sketch.background(20);
     }
 
     sketch.draw = function () {
         sketch.loadPixels();
+
+        maxIterations_Mandelbrot = iterSlider_mandelbrot.value();
+
         for (let y = 0; y < height; y++) {
             for (let x = 0; x < width; x++) {
 
-                let_a = sketch.map(x, 0, width, minVal, maxVal);
-                let b = sketch.map(y, 0, height, minVal, maxVal);
+                let i = calculateMandelbrotSet(x, y, maxIterations_Mandelbrot, sketch);
 
-                let c_a = a;
-                let c_b = b;
+                // let c_a = a;
+                // let c_b = b;
 
-                let i = 0;
+                // let i = 0;
 
-                while (i < maxIterations) {
-                    let a_new = _a * a - b * b;
-                    let b_new = 2 * a * b;
-                    a = a_new + c_a;
-                    b = b_new + c_b;
+                // while (i < maxIterations_Mandelbrot) {
+                //     let a_new = a * a - b * b;
+                //     let b_new = 2 * a * b;
+                //     a = a_new + c_a;
+                //     b = b_new + c_b;
 
-                    if (a + b > 16) {
-                        break;
-                    }
+                //     if (sketch.abs(a + b) > 4) {
+                //         break;
+                //     }
 
-                    i++;
-                }
+                //     i++;
+                // }
 
                 //giver colorvalue baseret på antal iterations
-                let color_val_mandel = sketch.map(i, 0, maxIterations, 0, 255)
+                let color_val_mandel = sketch.map(i, 0, maxIterations_Mandelbrot, 0, 255)
 
-                colorPixel(x, y, color_val_mandel, color_val_mandel, color_val_mandel, 255, sketch);
+                colorPixel(x, y, color_val_mandel, color_val_mandel, color_val_mandel, sketch);
+
+
+            }
+        }
+        sketch.updatePixels();
+        sketch.mouseClicked = function () {
+
+            if (sketch.mouseX > 0 && sketch.mouseX < width && sketch.mouseY > canvasOffset && sketch.mouseY < height + canvasOffset) {
+                getConstantValues();
+                UI_marker(sketch.mouseX, sketch.mouseY, sketch)
             }
         }
 
-        sketch.updatePixels();
+        function getConstantValues() {
+            cRe = sketch.map(sketch.mouseX, 0, width, minVal, maxVal);
+            cIm = sketch.map(sketch.mouseY, 0, height, minVal, maxVal);
+        }
     }
 
 
+    function UI_marker(x_pos, y_pos, sketch) {
+        sketch.fill("red");
+        sketch.strokeWeight(2);
+        sketch.ellipse(x_pos, y_pos, 10);
+        console.log(sketch.mouseX, sketch.mouseY);
+    }
 }
 
 new p5(mandelbrotSet);
+
+// let c_a = a;
+// let c_b = b;
+
+// let i = 0;
+
+// while (i < maxIterations_Mandelbrot) {
+//     let a_new = a * a - b * b;
+//     let b_new = 2 * a * b;
+//     a = a_new + c_a;
+//     b = b_new + c_b;
+
+//     if (sketch.abs(a + b) > 4) {
+//         break;
+//     }
+
+//     i++;
+// }
+
+function calculateMandelbrotSet(x, y, maxDepth, sketch) {
+
+    a = sketch.map(x, 0, width, minVal, maxVal);
+    b = sketch.map(y, 0, height, minVal, maxVal);
+
+    const c_a = a;
+    const c_b = b;
+
+    function recurseMandelbrotSet(a, b, cDepth) {
+        if (cDepth == maxDepth) {
+            return maxDepth;
+        }
+
+        const a_new = a * a - b * b;
+        const b_new = 2 * a * b;
+
+        a = a_new + c_a;
+        b = b_new + c_b;
+
+        if (sketch.abs(a + b) > 4) {
+            return cDepth;
+        }
+
+        return recurseMandelbrotSet(a, b, cDepth + 1)
+    }
+
+
+
+    return recurseMandelbrotSet(a, b, 0);
+}
+
